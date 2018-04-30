e927fc2ec3d9e47dd70c6e59e6f3c257af057b1d
Judy Zhang <judyz@andrew.cmu.edu>
2018-04-30 23:58:30 +0000

    made articles

---
+++ app/models/article.rb
@@ -0,0 +1,8 @@
+class Article < ApplicationRecord
+    belongs_to :category
+    
+    validate_presence_of :title, :content
+    
+    scope :active, 	     -> { where('active = ?', true) }
+    scope :alphabetical, -> { order('title') }
+end
